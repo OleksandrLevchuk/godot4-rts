dfcc321ce52245e7a682bd5fb6007f04c02e60fd
anexlader <anexlader@gmail.com>
2023-08-23 16:24:22 +0300

    15 hours in
    learned to make animations in clip studio
    learned to export animation sprite sheets
    learned the godot animation player
    made the tank move, added the track animation.

--- unit.gd
+++ unit.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
-@export var selected = false
+@export var is_selected = false
 @onready var box = get_node('selectbox')
+@onready var anim = get_node('AnimationPlayer')
+@onready var target = position
+var follow_cursor = false
+var speed = 100
+
+func _ready():
+	anim.speed_scale = 2.0
 
 func set_selected(value):
+	is_selected = value
 	box.visible = value
 
-var follow_cursor = false
-var speed = 50
-var target
 
 func _input(event):
 	if event.is_action_pressed('right_click'):
@@ -18,10 +23,11 @@ func _input(event):
 		
 func _physics_process(_delta):
 	if follow_cursor == true:
-		if selected:
+		if is_selected:
 			target = get_global_mouse_position()
+			anim.play('drive')
 	velocity = position.direction_to(target) * speed
 	if position.distance_to(target) > 15:
 		move_and_slide()
 	else:
-		pass
+		anim.stop()
