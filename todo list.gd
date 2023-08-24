de287629d2455ff4f8418f208840c6bc44105588
anexlader <anexlader@gmail.com>
2023-08-24 22:45:08 +0300

    Painted a resource

--- todo list.gd
+++ todo list.gd
@@ -1,11 +1,14 @@
 extends Node
-var done = '
-change the tank orientation as it moves
-'
-var todo = '
-make the tank have a gradual acceleration, with reduced turn rate at low and high speeds
-try to make a photoshop like navigation in godot
-make the tank turret separate for future animations
-look into pathfinding...
-maybe precalculate destinations of units in moving groups
-'
+var done = "
+	change the tank orientation as it moves
+"
+var todo = "
+	make some visual at the point you click, where the tank heads to
+	now there's only a drag selection. implement a single unit selection too
+	instead of a constant speed, make the tank accelerate gradually, with reduced turn rate at high speeds
+	deal with that terrible aliasing, like wtf
+	try to make a photoshop like navigation in godot
+	make the tank turret separate for future animations
+	look into pathfinding...
+	maybe precalculate destinations of units in moving groups
+"
--- unit.gd
+++ unit.gd
@@ -5,11 +5,8 @@ extends CharacterBody2D
 @onready var destination = position
 @export var is_selected = false
 var is_moving = false
-var is_rotating = false
 var max_speed = 200
 var rotation_speed = PI / 1 # how many seconds it takes to do a 180 degrees turn
-var desired_rotation = 0
-var rotation_threshold = deg_to_rad( 1 )
 
 func _ready():
 	animation.speed_scale = 2.0
@@ -23,23 +20,17 @@ func _input(event):
 	if event.is_action_pressed('right_click'):
 		destination = get_global_mouse_position()
 		is_moving = true
-		is_rotating = true
 		animation.play('drive')
-		
-func get_rotation_direction(start_angle: float, target_angle: float) -> int:
-	var diff = target_angle - start_angle
-	return sign(diff) if abs(diff) < PI else -sign(diff)
-	
+
 func _physics_process(delta):
-	if not is_moving: # no orders has been given yet
-		return # so do nothing
-	desired_rotation = position.angle_to_point(destination)
-	var difference = wrapf( desired_rotation - rotation, -PI, PI ) # wrapf allows subtracting angles without bugs
-#	var difference = asin(sin(rotation-desired_rotation)) # this also should work ???
-	if abs( difference ) > deg_to_rad( 1 ): # if the difference is too big
-		rotation = wrapf( rotation + rotation_speed * delta * sign(difference), -PI, PI ) # rotates in the direction of the difference
+	if not is_moving: return # no orders has been given yet
+	var desired_rotation = position.angle_to_point(destination)
+#	var difference = wrap( desired_rotation - rotation, -PI, PI ) # wrap allows subtracting angles without bugs
+	var difference = asin( sin( desired_rotation - rotation ) ) # this also works... less readable but probably faster
+	if abs( difference ) > deg_to_rad( 1 ): # if the difference is too big, turn the unit!
+		rotation = wrap( rotation + rotation_speed * delta * sign(difference), -PI, PI ) # rotates in the direction of the difference
 	velocity = Vector2( cos(rotation), sin(rotation) ) * max_speed
 	move_and_slide()
-	if position.distance_to(destination) < 25: # we've arrived, let's stop
+	if position.distance_to(destination) < 15: # we've arrived, let's stop
 		is_moving = false
 		animation.stop()
