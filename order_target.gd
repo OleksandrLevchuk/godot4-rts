b868831549782d10d04285a7a4ee2e3e0bc3e617
anexlader <anexlader@gmail.com>
2023-08-28 15:45:00 +0300

    now cursor stays fixed as the camera zooms
    fiddling with camera took me like 7 hours :(
    total time is 40 hours now

--- order_target.gd
+++ order_target.gd
@@ -1,19 +1,18 @@
 extends Node2D
 @onready var center = get_viewport().get_visible_rect().size/2
 var target = Vector2.ZERO
-const color1 = Color.MEDIUM_SPRING_GREEN
-const color2 = Color(Color.PURPLE,0)
+const color1 = Color.MEDIUM_SPRING_GREEN # this color is opaque
+const color2 = Color(Color.PURPLE,0) # alpha zero means that this color is fully transparent
 const fade_time = 0.5
 var fade_percent = 0
 var is_fading = false
 
 func _draw():
-	draw_circle(target, 20*fade_percent, color1.lerp(color2,fade_percent))
+	draw_circle(target,20*fade_percent,color1.lerp(color2,fade_percent))
 
 func _process(delta):
 	if Input.is_action_just_pressed("right_click"):
 		target = get_global_mouse_position()
-		print('circle position is ', target)
 		is_fading = true
 		fade_percent = 0
 	if is_fading:
