36a824e4ed51dce10bdcad4c61325a276c475c2e
anexlader <anexlader@gmail.com>
2023-08-29 23:09:26 +0300

    painted a building sketch
    tried using a very wide perspective angle,
    but it looks like I will have to fallback to isometry

---
+++ global/math cheatsheet.gd
@@ -0,0 +1,1 @@
+# to find angle and distance between two points, just do: point B - point A
--- global/todo list.gd
+++ global/todo list.gd
@@ -1,21 +1,23 @@
-extends Node
-var done = "
-	change the tank orientation as it moves
-	make some visual at the point you click, where the tank heads to
-	optimize the way the select box sends a signal to the world node indirectly through the ui node
-	fix the tank trembling when going in a straight line
-"
-var todo = "
-	fix the tank forever trying to go through walls
-	fix the tank running circles when the target is too close
-	organize the fking project folder, maybe put scripts into a separate one
-	i want triangular selection boxes
-	i want some verticality
-	as of now, there's only a drag selection. implement a single unit selection too
-	instead of a constant speed, make the tank accelerate gradually, with reduced turn rate at high speeds
-	deal with that terrible aliasing, like wtf. docs.godotengine.org/en/stable/tutorials/2d/2d_antialiasing.html
-	try to make a photoshop like navigation in godot
-	make the tank turret separate for future animations
-	look into pathfinding...
-	maybe precalculate destinations of units in moving groups, try making a visual aid first
-"
+########################################################
+#		DONE:
+#	change the tank orientation as it moves
+#	make some visual at the point you click, where the tank heads to
+#	optimize the way the select box sends a signal to the world node indirectly through the ui node
+#	fix the tank trembling when going in a straight line
+###########################################################
+#		TO DO:
+#	fix the tank forever trying to go through walls
+#	teach the tank to drive in reverse
+#	fix the tank sometimes flipping instantly when ordered to go slightly backwards
+#	fix the t/ank running circles when the target is too close
+#	organize the fking project folder, maybe put scripts into a separate one
+#	i want triangular selection boxes
+#	i want some verticality
+#	as of now, there's only a drag selection. implement a single unit selection too
+#	instead of a constant speed, make the tank accelerate gradually, with reduced turn rate at high speeds
+#	deal with that terrible aliasing, like wtf. docs.godotengine.org/en/stable/tutorials/2d/2d_antialiasing.html
+#	try to make a photoshop like navigation in godot
+#	make the tank turret separate for future animations
+#	look into pathfinding...
+#	maybe precalculate destinations of units in moving groups, try making a visual aid first
+#############################################################
