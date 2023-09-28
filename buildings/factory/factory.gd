df42b459072e28b0e1126ecb855a36b208ee81b8
anexlader <anexlader@gmail.com>
2023-09-28 08:35:15 +0300

    implemented a hover visual
    129 hours

--- buildings/factory/factory.gd
+++ buildings/factory/factory.gd
@@ -3,11 +3,12 @@ extends StaticBody2D
 #@onready var spawn_dialog := preload("res://ui/unit_spawn_dialog.tscn")
 #var is_hovered := false
 #signal selected
-#
+
+
 #func _ready():
 #	selected.connect( get_parent()._on_selected )
-#
-#
+
+
 #func _input( event:InputEvent ):
 #	if not event.is_action_released("left_click"): return
 #	if is_hovered:
@@ -15,9 +16,11 @@ extends StaticBody2D
 #		selected.emit(position, spawn_dialog)
 #	else:
 #		$selectbox.visible = false
-#
+
+
 #func _on_mouse_exited():
 #	is_hovered = false
-#
+
+
 #func _on_mouse_entered():
 #	is_hovered = true
--- components/SelectionComponent.gd
+++ components/SelectionComponent.gd
@@ -4,9 +4,9 @@ class_name SelectionComponent
 @export var main_sprite : Sprite2D
 @export var collider : CollisionShape2D
 @export var health_component : HealthComponent
-#@export var selectbox : Node
 
-@onready var selectbox := $Sprite2D
+@onready var select_sprite := $TransformParent/SelectionSprite
+@onready var hover_sprite := $TransformParent/HoverSprite
 
 var is_selected := false
 
@@ -15,18 +15,28 @@ signal deselected
 
 
 func _ready():
-	get_parent().mouse_entered.connect(select)
-	get_parent().mouse_exited.connect(deselect)
+	get_parent().mouse_entered.connect(hover)
+	get_parent().mouse_exited.connect(unhover)
 	var size = main_sprite.texture.get_size()
 	var new_scale = 0.1 + ( size.x + size.y ) / 5000
-	selectbox.scale.x = new_scale
-	selectbox.scale.y = new_scale
+	select_sprite.scale = Vector2( new_scale, new_scale)
+	new_scale -= 0.1
+	hover_sprite.scale = Vector2( new_scale, new_scale)
 
 	if health_component:
 		selected.connect(health_component._on_selected)
 		deselected.connect(health_component._on_deselected)
 
 #func _process(_delta):
+
+
+func hover():
+	hover_sprite.visible = true
+
+
+func unhover():
+	hover_sprite.visible = false
+
 
 func select():
 	set_selected(true)
@@ -40,4 +50,4 @@ func deselect():
 
 func set_selected(value:bool):
 	is_selected = value
-	selectbox.visible = value
+	select_sprite.visible = value
