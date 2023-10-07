647c1a9c5745dc20077e830d8cc6268cd8dd7472
anexlader <anexlader@gmail.com>
2023-10-07 18:20:45 +0300

    started making the gatherer/gatherable components
    also created a charger unit
    186 hours

--- components/GatherableComponent.gd
+++ components/GatherableComponent.gd
@@ -1,26 +1,18 @@
 extends Area2D
 class_name GatherableComponent
 
-@export_enum("Energy","Crystals","Credits") var type
+@export_enum("Energy","Crystals","Credits") var type : String
 @export var timer : Timer
-@export var health_component : HealthComponent
 
 var units_gathering := 0
 
 
-func damage( attack ):
-	if health_component:
-		health_component.damage(attack)
-
-
-#func _get_configuration_warnings():
-#	return ["test"]
+#func _get_configuration_warnings(): return ["test"]
 
 
 func _on_body_entered(body):
-	if not body.is_in_group('units'): return
+	if not body.has_node('%GathererComponent'): return
 	print('entered ', body)
-	damage( 1 )
 	if units_gathering == 0:
 		timer.start()
 	units_gathering += 1
@@ -34,4 +26,4 @@ func _on_body_exited(body):
 
 
 func _on_gathering_timer_timeout():
-	damage( units_gathering )
+	pass
---
+++ components/GathererComponent.gd
@@ -0,0 +1,5 @@
+extends ProgressBar
+class_name GathererComponent
+
+@export_enum("Energy","Crystals","Credits") var type
+
