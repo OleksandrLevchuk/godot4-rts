5fe99168084f8a95d1a3d55983bf1dbb74e68efa
anexlader <anexlader@gmail.com>
2023-10-09 11:01:14 +0300

    i'm remaking the movement component again
    for like 6th time. this time the idea is to draw movement trajectories for debugging purposes
    194 hours

--- components/MovementComponent.gd
+++ components/MovementComponent.gd
@@ -1,63 +1,28 @@
 extends Timer
 class_name MovementComponent
 
-## driving units can't strife sideways
-@export_enum("Wheeled", "Legged") var movement_type : String = "Wheeled"
-@export var MAX_SPEED : float = 200
-## seconds to reach max speed
-@export var ACCEL_TIME : float = 1
-## seconds to turn around at full speed
-@export var TURN_RATE : float = 1
-@export var MINIMAP_UPDATE_RATE : float = 1
-@export var SPEED_CURVE : Curve
-@export var DEBUG: bool = true 
-
-var is_turning := false
-var is_accelerating := false
-var is_braking := false
-var velocity: Vector2
-var destination: Vector2
-var speed_mult := 0.0
-var braking_distance : float = 0.0
-var elapsed := 0.0
 var velo := preload("res://components/VelocityCalc.gd").new()
 
-@onready var line: Line2D = Line2D.new()
-@onready var parent := get_owner()
-@onready var prev_pos: Vector2 = parent.position
+@onready var parent = get_owner()
 
-signal started_moving
-signal minimap_updated
-signal stopped_moving
-
+signal arrived
+signal map_moved
 
 func _ready():
-	velo.halted.connect(_on_halted)
-	timeout.connect(func():minimap_updated.emit(parent.position))
-	if has_node("%AnimationPlayer"):
-		started_moving.connect(func():%AnimationPlayer.play('drive'))
-		stopped_moving.connect(%AnimationPlayer.stop)
-	velocity = Vector2.ONE.rotated(parent.rotation)
+	velo.arrived.connect(_on_arrived)
 
 
 func _process(delta):
-	parent.velocity = velo.calc(parent.position, parent.velocity, delta)
-	parent.rotation = parent.velocity.angle()
+	parent.velocity = velo.calc(delta)
 	parent.move_and_slide()
 
 
-func _on_ordered_to_move(dest):
-	start()
-	set_process(true)
-	destination = dest
-	is_braking = false
-	is_accelerating = true
-	is_turning = true
-	started_moving.emit()
+func _on_ordered_to_move( dest ):
+	velo.init( parent.position, parent.rotation, dest )
+	set_process(true) # enable the process method
 
 
-func _on_halted():
-	stop() # stop the minimap update timer
-	minimap_updated.emit(parent.position) # update the minimap one last time
-	set_process(false) # stop the _process method
-	stopped_moving.emit() # notify subscribers, e.g. gather or attack components
+func _on_arrived():
+	arrived.emit()
+	map_moved.emit()
+	set_process(false) # disable the process method
--- components/VelocityCalc.gd
+++ components/VelocityCalc.gd
@@ -1,42 +1,32 @@
+const ACCEL_TIME: float = 3
+const MAX_SPEED: float = 200
 
-func calc(pos, velo, delta):
+var position: Vector2
+var velocity_normal: Vector2
+var velocity: Vector2
+var elapsed: float = 0
+var is_accelerating: bool = false
+var is_turning: bool = false
+
+signal arrived
+
+
+func init(pos, angle, dest):
+	position = pos
+	velocity_normal = Vector2.RIGHT.rotated(angle)
+	is_accelerating = true
+	is_turning = true
+
+
+func calc(delta):
 	if is_accelerating:
 		elapsed += delta
 		if elapsed > ACCEL_TIME:
+			elapsed = ACCEL_TIME
 			is_accelerating = false
-		braking_distance += parent.position.distance_to(prev_pos)
-		print(braking_distance)
-		prev_pos = parent.position
-		speed_mult = SPEED_CURVE.sample_baked(elapsed/ACCEL_TIME)
-		velocity = velocity.normalized() * MAX_SPEED * speed_mult
-		parent.velocity = velocity
+		velocity = velocity_normal * elapsed/ACCEL_TIME * MAX_SPEED
 
-	elif is_braking:
-		elapsed -= delta
-		if elapsed < 0:
-			is_braking = false
-			set_process(false)
-			stopped_moving.emit()
-			map_moved.emit(parent.position)
-			stop()
-		speed_mult = SPEED_CURVE.sample_baked(elapsed/ACCEL_TIME)
-		velocity = velocity.normalized() * MAX_SPEED * speed_mult
-		parent.velocity = velocity
+#	if is_turning:
+		
 
-	if is_turning:
-		if velocity == Vector2.ZERO:
-			velocity = Vector2.RIGHT.rotated(parent.rotation) / 999
-		var facing: Vector2 = velocity.normalized()
-		var facing_wanted: Vector2 = (destination-parent.position).normalized()
-		var which_side: float = sign(facing.cross( facing_wanted ))
-		velocity = velocity.rotated( delta/TURN_RATE * PI * which_side )
-		if facing.dot(facing_wanted) > facing.dot(velocity.normalized()):
-			is_turning = false
-			velocity = facing_wanted * velocity.length()
-		parent.rotation = velocity.angle()
-		parent.velocity = velocity
-
-	if not is_braking and parent.position.distance_to(destination)<braking_distance:
-		braking_distance = 0.0
-		is_accelerating = false
-		is_braking = true
+	return velocity
