7312b3422f3ae916f2881a0f65fb7d6dea1298ba
anexlader <anexlader@gmail.com>
2023-10-20 11:50:28 +0300

    started implementing the turret bullet traces
    also converted a lot of script files into built-in scripts
    222 hours

--- components/ControllerComponent.gd
+++ components/ControllerComponent.gd
@@ -11,10 +11,10 @@ signal ordered_to_gather
 
 
 func _ready():
-	if can_attack:
-		ordered_to_attack.connect(%AttackComponent._on_ordered_to_attack)
 	if can_move:
 		ordered_to_move.connect(%MovementComponent._on_ordered_to_move)
+	if can_attack:
+		ordered_to_attack.connect(%AttackComponent._on_ordered_to_attack)
 	if can_gather:
 		ordered_to_gather.connect(%GatherComponent._on_ordered_to_gather)
 	# at first units are unselected and can't be controlled
@@ -26,16 +26,17 @@ func _input(event):
 		var query := PhysicsPointQueryParameters2D.new()
 		query.position = get_global_mouse_position()
 		var targets = get_world_2d().direct_space_state.intersect_point(query)
-		for unit in targets:
-			if unit.collider==get_owner(): continue
-			if can_attack and unit.collider.has_node("%HealthComponent"):
-				ordered_to_attack.emit(unit.collider)
+		for target in targets:
+			target = target.collider
+			if target==get_owner(): continue
+			if can_attack and target.has_node("%HealthComponent"):
+				ordered_to_attack.emit(target)
 				return # if a unit is found - attack, if not - move
-			if can_gather and unit.collider.has_node('%GatherableComponent'):
-				ordered_to_gather.emit(unit.collider)
+			if can_gather and target.has_node('%GatherableComponent'):
+				ordered_to_gather.emit(target)
 				return
-		if can_move:
-			ordered_to_move.emit(get_global_mouse_position())
+		if can_move: # reaching this point means we haven't found units here
+			ordered_to_move.emit( get_global_mouse_position() )
 
 
 func _on_selected(): # enable control for selected units
